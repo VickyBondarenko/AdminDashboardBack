d1eb65cf1387bca9e739eb541a60a489035bf90a
VickyBondarenko <quikyv@gmail.com>
2023-10-17 18:47:56 +0200

    renove ayth log

--- src/controllers/auth-controllers.js
+++ src/controllers/auth-controllers.js
@@ -75,7 +75,6 @@ const logout = async (req, res) => {
 };
 
 module.exports = {
-  // register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
